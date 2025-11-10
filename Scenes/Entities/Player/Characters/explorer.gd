a8002f0779654ee1a459541f9a1077560b8e9564
Aaron Huggins <achuggins.dev@gmail.com>
2025-11-09 17:51:13 -0700

    Character Swapping
    - Implement the basis for character swapping by adding functionality for prefab creation and deletion.
    - Create basic shapes as placeholders for character images.
    - Remove player.gd inheritance from explorer.gd.
    - Modify default viewport to 1280x720

--- Scenes/Entities/Player/Characters/explorer.gd
+++ Scenes/Entities/Player/Characters/explorer.gd
@@ -1,18 +1,14 @@
-extends "res://Entities/Player/player.gd"
+extends Area2D
 
 @export var speed = 400
 var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	super()
-	
 	screen_size = get_viewport_rect().size
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	super(delta)
-	
 	Move(delta)
 
 func Move(delta):
---
+++ Scenes/Entities/Player/player.gd
@@ -0,0 +1,81 @@
+extends Node2D
+
+# SIGNALS
+
+signal died
+
+# REFERENCES
+
+@export var explorer: PackedScene
+@export var fighter: PackedScene
+@export var worker: PackedScene
+
+var active_character: Node
+
+# VALUES
+
+@export var max_health: int = 5
+var health: int
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	assert(explorer != null, "Explorer not set.")
+	assert(fighter != null, "Fighter not set.")
+	assert(worker != null, "Worker not set.")
+	
+	health = max_health
+	switch_character("Explorer")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("Test_Damage"):
+		damage(1)
+	
+	if Input.is_action_just_pressed("Test_Heal"):
+		heal(1)
+	
+	switch_character()
+
+func switch_character(name: String = ""):
+	var new_character = null
+	if Input.is_action_just_pressed("Test_Select_Explorer") or name == "Explorer":
+		new_character = explorer.instantiate()
+	elif Input.is_action_just_pressed("Test_Select_Fighter") or name == "Fighter":
+		new_character = fighter.instantiate()
+	elif Input.is_action_just_pressed("Test_Select_Worker") or name == "Worker":
+		new_character = worker.instantiate()
+	else:
+		return
+	
+	if(active_character != null):
+		active_character.queue_free()
+	
+	active_character = new_character
+	add_child(active_character)
+	active_character.position = position
+
+func damage(amount: int):
+	if(amount <= 0):
+		return
+	
+	health -= amount
+	if(health <= 0):
+		die()
+	
+	print("Health: " + str(health))
+
+func heal(amount: int):
+	if(amount <= 0):
+		return
+	
+	health += amount
+	if(health > max_health):
+		health = max_health
+	
+	print("Health: " + str(health))
+
+func die():
+	hide()
+	died.emit()
+	print("died")
